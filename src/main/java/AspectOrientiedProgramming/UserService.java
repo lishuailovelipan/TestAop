7bf77550885be4e334d696a9a091c9b7817f9bcd
lishuailovelipan <19165455214@sina.cn>
2019-06-12 11:27:25 +0800

    第一次提交代码

---
+++ src/main/java/AspectOrientiedProgramming/UserService.java
@@ -0,0 +1,17 @@
+
+/**
+* @author 作者 :lishuai
+* @version 创建时间：2019年5月22日 上午11:16:04
+* 类说明
+*/
+
+package AspectOrientiedProgramming;
+
+
+public class UserService {
+
+	public void userLogin(){
+		System.out.println("我是用来登陆的");
+	}
+}
+
