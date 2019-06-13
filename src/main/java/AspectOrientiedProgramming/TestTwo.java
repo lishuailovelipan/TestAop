e201cc76eb462513d796d4febabbcaf2139d101b
lishuailovelipan <19165455214@sina.cn>
2019-06-13 09:51:37 +0800

    TestTwo提交

---
+++ src/main/java/AspectOrientiedProgramming/TestTwo.java
@@ -0,0 +1,22 @@
+
+/**
+* @author 作者 :lishuai
+* @version 创建时间：2019年6月13日 上午9:50:47
+* 类说明
+*/
+
+package AspectOrientiedProgramming;
+
+public class TestTwo {
+
+	/**
+	 * @Title: main @Description 方法的作用 : TODO @param @param args 参数 : @return
+	 * void 返回类型 : @throws
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+}
